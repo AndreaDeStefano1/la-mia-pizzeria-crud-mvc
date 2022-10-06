7a843d7e0b7751f838e43e79d2d0c478f1e55d17
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-10-06 12:48:30 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,70 @@
+﻿using la_mia_pizzeria.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics;
+
+
+namespace la_mia_pizzeria.Controllers
+{
+    public class PizzaController : Controller
+    {
+        private readonly ILogger<PizzaController> _logger;
+
+        public PizzaController(ILogger<PizzaController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            
+            using (PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> pizze = db.Pizzas.OrderByDescending(pizza => pizza.Id).ToList<Pizza>();
+                return View(pizze);
+            }
+            
+        }
+        public IActionResult Show(int id)
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                Pizza p = db.Pizzas.Where(s => s.Id == id).First<Pizza>();
+                return View(p);
+
+            }
+
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza pizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", pizza);
+            }
+            using (PizzaContext db = new PizzaContext())
+            {
+                Pizza p = new Pizza();
+                p.Name = pizza.Name;
+                p.Description = pizza.Description;
+                p.Image = pizza.Image;
+                p.Price = pizza.Price;
+                db.Add(p);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,40 @@
+﻿using la_mia_pizzeria.Utils.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria.Models
+{
+    public class Pizza
+    {
+        
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Il campo nome è obbligatorio")]
+        [StringLength(25, ErrorMessage = "Il nome non può avere più di 25 caratteri")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Il campo nome è obbligatorio")]
+        [StringLength(255, ErrorMessage = "Il nome non può avere più di 255 caratteri")]
+        [MinimumFiveWord]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Il campo descrizione è obbligatorio")]
+        public string Image { get; set; }
+
+        [Required(ErrorMessage = "Il campo prezzo è obbligatorio")]
+        [NotNegativeOrZero]
+        public double Price { get; set; }
+
+        public Pizza()
+        {
+        }
+
+        public Pizza(int id, string name, string description, string image, double price)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
+    }
+}
